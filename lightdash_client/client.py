e47a8a170f80a944cd5bcd77221d4aec9a467333
Yu ISHIKAWA <yuu.ishikawa+github@gmail.com>
2023-06-01 09:05:13 +0900

    Generate the client with the updated JSON schema
    Signed-off-by: Yu ISHIKAWA <yuu.ishikawa+github@gmail.com>

--- lightdash_client/client.py
+++ lightdash_client/client.py
@@ -58,7 +58,7 @@ class AuthenticatedClient(Client):
     """A Client which has been authenticated for use on secured endpoints"""
 
     token: str
-    prefix: str = "ApiKey"
+    prefix: str = "Bearer"
     auth_header_name: str = "Authorization"
 
     def get_headers(self) -> Dict[str, str]:
