17b6e7108e5aac4b4ebdb3340a597329861527af
Yu Ishikawa <yu-iskw@users.noreply.github.com>
2023-06-19 21:40:30 +0900

    Use 0.619.1 (#11)
    Signed-off-by: Yu ISHIKAWA <yuu.ishikawa+github@gmail.com>

--- lightdash_client/models/organization_member_profile_update.py
+++ lightdash_client/models/organization_member_profile_update.py
@@ -3,57 +3,48 @@ from typing import Dict
 from typing import List
 from typing import Type
 from typing import TypeVar
-from typing import Union
 
 import attr
 
 from ..models.organization_member_role import OrganizationMemberRole
-from ..types import UNSET
-from ..types import Unset
 
-T = TypeVar("T", bound="PartialPickOrganizationMemberProfileRole")
+T = TypeVar("T", bound="OrganizationMemberProfileUpdate")
 
 
 @attr.s(auto_attribs=True)
-class PartialPickOrganizationMemberProfileRole:
-    """Make all properties in T optional
-
+class OrganizationMemberProfileUpdate:
+    """
     Attributes:
-        role (Union[Unset, OrganizationMemberRole]):
+        role (OrganizationMemberRole):
     """
 
-    role: Union[Unset, OrganizationMemberRole] = UNSET
+    role: OrganizationMemberRole
     additional_properties: Dict[str, Any] = attr.ib(init=False, factory=dict)
 
     def to_dict(self) -> Dict[str, Any]:
-        role: Union[Unset, str] = UNSET
-        if not isinstance(self.role, Unset):
-            role = self.role.value
+        role = self.role.value
 
         field_dict: Dict[str, Any] = {}
         field_dict.update(self.additional_properties)
-        field_dict.update({})
-        if role is not UNSET:
-            field_dict["role"] = role
+        field_dict.update(
+            {
+                "role": role,
+            }
+        )
 
         return field_dict
 
     @classmethod
     def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
         d = src_dict.copy()
-        _role = d.pop("role", UNSET)
-        role: Union[Unset, OrganizationMemberRole]
-        if isinstance(_role, Unset):
-            role = UNSET
-        else:
-            role = OrganizationMemberRole(_role)
+        role = OrganizationMemberRole(d.pop("role"))
 
-        partial_pick_organization_member_profile_role = cls(
+        organization_member_profile_update = cls(
             role=role,
         )
 
-        partial_pick_organization_member_profile_role.additional_properties = d
-        return partial_pick_organization_member_profile_role
+        organization_member_profile_update.additional_properties = d
+        return organization_member_profile_update
 
     @property
     def additional_keys(self) -> List[str]:
--- lightdash_client/models/pick_allowed_email_domains_exclude_keyof_allowed_email_domains_organization_uuid.py
+++ lightdash_client/models/pick_allowed_email_domains_exclude_keyof_allowed_email_domains_organization_uuid.py
@@ -17,20 +17,20 @@ class PickAllowedEmailDomainsExcludeKeyofAllowedEmailDomainsOrganizationUuid:
     """From T, pick a set of properties whose keys are in the union K
 
     Attributes:
-        role (OrganizationMemberRole):
         email_domains (List[str]):
+        role (OrganizationMemberRole):
         project_uuids (List[str]):
     """
 
-    role: OrganizationMemberRole
     email_domains: List[str]
+    role: OrganizationMemberRole
     project_uuids: List[str]
     additional_properties: Dict[str, Any] = attr.ib(init=False, factory=dict)
 
     def to_dict(self) -> Dict[str, Any]:
-        role = self.role.value
-
         email_domains = self.email_domains
+
+        role = self.role.value
 
         project_uuids = self.project_uuids
 
@@ -38,8 +38,8 @@ class PickAllowedEmailDomainsExcludeKeyofAllowedEmailDomainsOrganizationUuid:
         field_dict.update(self.additional_properties)
         field_dict.update(
             {
-                "role": role,
                 "emailDomains": email_domains,
+                "role": role,
                 "projectUuids": project_uuids,
             }
         )
@@ -49,15 +49,15 @@ class PickAllowedEmailDomainsExcludeKeyofAllowedEmailDomainsOrganizationUuid:
     @classmethod
     def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
         d = src_dict.copy()
-        role = OrganizationMemberRole(d.pop("role"))
-
         email_domains = cast(List[str], d.pop("emailDomains"))
+
+        role = OrganizationMemberRole(d.pop("role"))
 
         project_uuids = cast(List[str], d.pop("projectUuids"))
 
         pick_allowed_email_domains_exclude_keyof_allowed_email_domains_organization_uuid = cls(
-            role=role,
             email_domains=email_domains,
+            role=role,
             project_uuids=project_uuids,
         )
 
