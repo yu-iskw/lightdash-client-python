8693c574eeba9562e7429f5b5780ba183df2f910
Yu Ishikawa <yu-iskw@users.noreply.github.com>
2024-08-02 13:07:25 +0900

    Update the schema
    Signed-off-by: Yu Ishikawa <yu-iskw@users.noreply.github.com>

--- dev/dereference_swagger_json.sh
+++ dev/dereference_swagger_json.sh
@@ -6,9 +6,13 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
 # Arguments
 docker_image="redocly/cli:latest"
+output_file="rebuilt-swagger.json"
 while (($# > 0)); do
   if [[ "$1" == "--image" ]]; then
     docker_image="${2}"
+    shift 2
+  elif [[ "$1" == "--output" ]]; then
+    output_file="${2}"
     shift 2
   else
     echo "WARN: unrecognized argument ${1}" >&2
@@ -16,8 +20,10 @@ while (($# > 0)); do
   fi
 done
 
+# Split the swagger.json into multiple files to handle recursive references
+docker run --rm -v "${SCRIPT_DIR:?}/schemas:/spec" "${docker_image:?}" \
+  split --outDir /spec/split-swagger /spec/swagger.json
 
-# NOTE The --output option doesn't work with swagger-cli. Instead, use the '-o' option.
-mounted_dir="/spec"
-docker run --rm -v "${SCRIPT_DIR:?}/schemas:${mounted_dir}" "${docker_image:?}" \
-  bundle --dereferenced "${mounted_dir:?}/swagger.json" | tee "${mounted_dir:?}/dereferenced-swagger.json"
+# Bundle the split swagger.json files into a single file again
+docker run --rm -v "${SCRIPT_DIR:?}/schemas:/spec" "${docker_image:?}" \
+  bundle --output "/spec/${output_file}" /spec/split-swagger/openapi.json
